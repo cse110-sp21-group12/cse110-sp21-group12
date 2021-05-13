179e32be3491f4264d9953d52f78a9cc317b8325
DustinLin <dustinylin@gmail.com>
2021-05-12 17:53:08 -0700

    inital setup and test of jsDoc

--- source/test.js
+++ source/test.js
@@ -1,1 +1,19 @@
-console.log('hello world');
+/**
+ * prints hello world!
+ */
+function helloWorld() {
+    console.log('hello world');
+}
+
+/**
+ * takes in 2 numbers and adds them
+ * @param {Number} a
+ * @param {Number} b
+ * @returns {Number} The sum of a and b
+ */
+function adder(a, b) {
+    return a + b;
+}
+
+helloWorld();
+adder(1, 1);
