28dec9c28c1533dc9a9c196ce6b7bd17183d7bd8
DustinLin <dustinylin@gmail.com>
2021-05-17 09:29:40 -0700

    prepare test.js for sample doc generation

---
+++ source/test.js
@@ -0,0 +1,19 @@
+/**
+ * prints hello world!
+ */
+function helloWorld() {
+    console.log('hello world');
+}
+
+/**
+ * takes the sum of 2 numbers
+ * @param {Number} x arg1
+ * @param {Number} y arg2
+ * @returns {Number} sum of x and y
+ */
+function sum(x, y) {
+    return x + y;
+}
+
+sum(1 + 2);
+helloWorld();
