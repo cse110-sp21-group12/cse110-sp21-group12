a598b5d9fb8997dc6476d2d6c75c39ffb5a85e66
Sasha <okubichk@ucsd.edu>
2021-05-19 19:03:11 -0700

    Add jsDocs style comments to the Login javascript

--- source/Login/scripts.js
+++ source/Login/scripts.js
@@ -1,12 +1,17 @@
+//store current page state
 var loginState = 'returning';
 
+//make the login button redirect to Index
 var login_button = document.getElementById('login-button');
 login_button.addEventListener('click', goHome);
 
+//make the toggle button change the page state
 var switch_button = document.getElementById('switch-screen');
-
 switch_button.addEventListener('click', toggleView);
 
+/**
+ * Handle toggling the state of the page, calls the setNewUser() and setReturningUser() helper functions
+ */
 function toggleView() {
     if (loginState == 'returning') {
         loginState = 'new';
@@ -17,11 +22,17 @@ function toggleView() {
     }
 }
 
+/**
+ * Redirect the browser to the Index page with a href
+ */
 function goHome() {
     //alert('login');
     window.location.href = '../Index/index.html';
 }
-
+/**
+ * Change the login screen to the "New User" mode
+ * Hide username, update text.
+ */
 function setNewUser() {
     document.getElementById('username').style.display = 'none';
     document.getElementById('title').innerText = 'Create your login!';
@@ -29,6 +40,10 @@ function setNewUser() {
     login_button.innerText = 'Sign-Up';
 }
 
+/**
+ * Change the login screen to the "Returning User" mode
+ * Show username, update text
+ */
 function setReturningUser() {
     document.getElementById('username').style.display = 'flex';
     document.getElementById('title').innerText = 'Welcome back!';
