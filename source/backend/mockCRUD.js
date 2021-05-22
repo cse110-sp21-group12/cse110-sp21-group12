1d062fce499021129435d5bb412b76bf05488e98
Sasha <okubichk@ucsd.edu>
2021-05-21 17:21:50 -0700

    Frontend handling of db calls for signup and login
    Co-authored-by: Keith Ho  <KeithDHo@users.noreply.github.com>

--- source/backend/mockCRUD.js
+++ source/backend/mockCRUD.js
@@ -4,3 +4,11 @@
 export function mockGETPasswordHash(){
     return 1083;
 }
+
+/**
+ * Mock function to add a new user record to the database
+ * @param {*} userObject A user object containing Username, Password Hash, and Theme ID
+ */
+export function mockPUTUser(userObject){
+    //
+}
