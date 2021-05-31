d8a2f34bac5082fb6d3eea2fef410232e9d422b7
Nhan Pham <n2pham@ucsd.edu>
2021-05-30 21:20:49 -0700

    Just some tests

--- source/tests/sampleP.test.js
+++ source/tests/sampleP.test.js
@@ -2,7 +2,7 @@
 
 const { loadPartialConfigAsync } = require('@babel/core');
 
-//sample puppeteer test
+/*//sample puppeteer test
 describe('Google', () => {
     beforeAll(async () => {
         await page.goto('https://google.com');
@@ -11,4 +11,21 @@ describe('Google', () => {
     it('should display "google" text on page', async () => {
         await expect(page).toMatch('google');
     });
+});*/
+//npm test source/tests/sampleP.test.js
+
+describe('basic navigation for BJ', () => {
+  beforeAll(async () => {                                
+    await page.goto('http://127.0.0.1:5501/source/Login/Login.html');
+    await page.waitForTimeout(500);
+
+//does it work????
+
+  it('Test1: Initial Home Page - Shows create your login ', async () => {
+    const headerText = await page.$eval('header', (header) => {
+      return header.innerHTML;
+    });
+    expect(headerText).toBe('Create your login!');
+  }); 
+
 });
