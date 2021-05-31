7ab191c9a9dac1fa6c12a24d015d3db4bbf3ec9a
DustinLin <dustinylin@gmail.com>
2021-05-30 21:27:44 -0700

    set up first test

--- source/tests/sampleP.test.js
+++ source/tests/sampleP.test.js
@@ -18,11 +18,10 @@ describe('basic navigation for BJ', () => {
   beforeAll(async () => {                                
     await page.goto('http://127.0.0.1:5501/source/Login/Login.html');
     await page.waitForTimeout(500);
-
-//does it work????
+  });
 
   it('Test1: Initial Home Page - Shows create your login ', async () => {
-    const headerText = await page.$eval('header', (header) => {
+    const headerText = await page.$eval('#title', (header) => {
       return header.innerHTML;
     });
     expect(headerText).toBe('Create your login!');
