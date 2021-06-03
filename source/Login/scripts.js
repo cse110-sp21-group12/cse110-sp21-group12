3decfc736f6f741332be252b6dc3d1c5ee57039b
Aaron <aaronkchao@yahoo.com>
2021-06-02 17:16:21 -0700

    fixed color customizations

--- source/Login/scripts.js
+++ source/Login/scripts.js
@@ -76,7 +76,7 @@ function handleSignup(newUsername, newPassword) {
     let userObject = {
         username: newUsername,
         password: newPassword,
-        theme: '#e4e4e4',
+        theme: '#BDFFBD',
     };
     //update settings
     // eslint-disable-next-line no-undef
