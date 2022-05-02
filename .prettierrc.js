d94881416ee1f641f4aa024f422fce40f91a5a52
DustinLin <dustinylin@gmail.com>
2022-05-02 12:08:09 -0700

    NL-14 adjusting workflow to run on appropirate branches, updated package.json

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 // prettier.config.js or .prettierrc.js
 
 /*
- * Here is the configeration for prettier,
+ * Here is the configuration for prettier,
  * link below has more information on what can be included
  * https://prettier.io/docs/en/options.html
  */
