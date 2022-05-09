450fd4a4eea1b7defa75b1f1cd3e1533c9449ab9
s1hu <50194274+s1hu@users.noreply.github.com>
2022-05-09 15:57:45 -0700

    Change valid_branch_regex < check_branch_naming.sh

--- .husky/hooks/check_branch_naming.sh
+++ .husky/hooks/check_branch_naming.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 local_branch_name="$(git rev-parse --abbrev-ref HEAD)"
 
-valid_branch_regex='^((fix|feat)\/[a-zA-Z0-9\-]+)$'
+valid_branch_regex='^NL-\d+\_\w.*-(Alice|Sophia|Dustin|Shawn|Rickesh|Justin|Nicholas|Christopher)(_Alice|_Sophia|_Dustin|_Shawn|_Rickesh|_Justin|_Nicholas|_Christopher)*$'
 
 message="There is something wrong with your branch name. Branch names in this project must adhere to this contract: $valid_branch_regex. Your commit will be rejected. You should rename your branch to a valid name and try again."
 
