98afea34f636554d11e8efb0a34fdc130575481d
DustinLin <dustinylin@gmail.com>
2022-05-10 21:59:03 -0700

    added file comments and removed testing code

--- .husky/hooks/testing.js
+++
@@ -1,2 +0,0 @@
-// this is a testing file to check the precommit linter
-console.log('hello world');
--- testing.js
+++
@@ -1,3 +0,0 @@
-// this is a testing file to check the precommit linter
-console.log("hello world")
-let not_used = 0;
