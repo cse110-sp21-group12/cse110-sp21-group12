89f6e8cce53068bfb5dbc84d4fd60ebe538b8e13
Rick10101221 <rikhilna@ucsd.edu>
2022-05-26 21:27:06 -0700

    docs: add documentation for backend functions + weekly panel/notes

--- source/Login/WeeklyOverview/WeeklyJS.js
+++ source/Login/WeeklyOverview/WeeklyJS.js
@@ -5,7 +5,7 @@ import {
 } from '../../Backend/BackendInit.js';
 
 /**
- * Loads the next 7 days worth of data into the weekly preview
+ * Loads the current week (7 days worth of data) into the weekly preview
  * @returns void
  */
 async function loadWeek(currWeekList) {
@@ -25,6 +25,7 @@ async function loadWeek(currWeekList) {
     // iterates through each day and creates list of tasks
     for (let i = 0; i < days.length; i++) {
         const currentDay = weekData[days[i]];
+        // if there is no data for the current day in the database
         if (currentDay === undefined) {
             continue;
         }
@@ -43,12 +44,22 @@ async function loadWeek(currWeekList) {
     todoList.append(line);
 }
 
+/**
+ * recursively build all the bullets for the passed-in bullet list and append
+ * the final list into the passed-in list
+ * @param {Object} bullets - object that contains the bullet notes for a
+ *                           particular day
+ * @param {HTMLElement:ul} list - unordered list where bullet elements (li) are
+ *                                appended to
+ */
 function bulletParser(bullets, list) {
     // eslint-disable-next-line no-unused-vars
     for (const [_, bullet] of Object.entries(bullets)) {
         const todoBullet = document.createElement('li');
         todoBullet.textContent = bullet.text;
 
+        // if a bullet is marked as completed, add a strikethrough. otherwise,
+        // render it normally
         if (bullet.done == true) {
             const strikeThrough = document.createElement('s');
             strikeThrough.appendChild(todoBullet);
@@ -57,6 +68,9 @@ function bulletParser(bullets, list) {
             list.append(todoBullet);
         }
 
+        // if there is a childList containing more bullets, begin rendering
+        // the childList and append those bullets to a new unordered-list
+        // that is nested under the parent unordered-list
         if ('childList' in bullet) {
             const subList = document.createElement('ul');
             bulletParser(bullet['childList'], subList);
